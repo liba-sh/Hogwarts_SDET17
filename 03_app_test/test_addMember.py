0ed415b8c6acfd0e749de84ee578363ace09fd2b
liba_sh <878737458@qq.com>
2021-03-15 21:20:42 +0800

    企业微信 app 项目实战1-更新-封装滑动

--- 03_app_test/test_addMember.py
+++ 03_app_test/test_addMember.py
@@ -13,6 +13,13 @@ class TestDemo:
         caps["appPackage"] = "com.tencent.wework"
         caps["appActivity"] = ".launch.LaunchSplashActivity"
         caps["noReset"] = "true"
+        #跳过安装uiautomator2server等服务
+        caps["skipServerInstallation"] = "true"
+        #跳过设备初始化
+        caps["skipDeviceInitialization"] = "true"
+        # 测试之前不停止app运行
+        # caps["dontStopAppOnReset"] = "true"
+
         #动态设置appium等待时长
         caps['settings[waitForIdleTimeout]'] = 1
         # 客户端与appium服务器建立连接的代码
@@ -22,9 +29,28 @@ class TestDemo:
     def teardown(self):
         self.driver.quit()
 
+    def swipe_find(self,text):
+        while True:
+            try:
+                element=self.driver.find_element(MobileBy.XPATH,f"//*[@text='{text}']")
+                return element
+            except:
+                print("未找到")
+                size=self.driver.get_window_size()
+                width=size.get('width')
+                height=size.get('height')
+                start_x=width/2
+                start_y=height*0.8
+                end_x=start_x
+                end_y=height*0.2
+                self.driver.swipe(start_x,start_y,end_x,end_y,1000)
+
+
     def test_addMember(self):
         self.driver.find_element(MobileBy.XPATH,"//*[@text='通讯录']").click()
-        self.driver.find_element(MobileBy.XPATH,"//*[@text='添加成员']").click()
+        # self.driver.find_element(MobileBy.XPATH,"//*[@text='添加成员']").click()
+        element=self.swipe_find("添加成员")
+        element.click()
         self.driver.find_element(MobileBy.XPATH,"//*[@text='手动输入添加']").click()
         self.driver.find_element(MobileBy.XPATH,"//android.widget.RelativeLayout//*[@text='必填']").send_keys("test003")
         self.driver.find_element(MobileBy.XPATH,"//*[@text='男']").click()
@@ -32,6 +58,9 @@ class TestDemo:
         self.driver.find_element(MobileBy.XPATH,"//*[@text='男']").click()
         self.driver.find_element(MobileBy.XPATH,"//*[@text='手机号']").send_keys("13855667703")
         self.driver.find_element(MobileBy.XPATH,"//*[@text='保存']").click()
+        #验证添加成员toast
+        #assert '添加成功' in self.driver.page_source
+        self.driver.find_element(MobileBy.XPATH,"//*[@text='添加成功']")
 
     def test_removeMember(self):
         self.driver.find_element(MobileBy.XPATH,"//*[@text='通讯录']").click()
